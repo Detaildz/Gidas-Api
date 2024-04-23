ab762ec382f392d6be44fdec60ac426ee3fe0839
Darjus <darjus.zukovski@gmail.com>
2024-04-24 01:24:28 +0300

    2

--- server.js
+++ server.js
@@ -10,6 +10,21 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 const server = http.createServer(app);
+
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://master--mano-gidas.netlify.app'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 const io = socketIo(server, {
   pingTimeout: 60000,
   cors: {
